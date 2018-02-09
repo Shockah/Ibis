8e67d39d7682dfc388e9196e1a4665ae05d26148
Michał Dolaś <me@shockah.pl>
2018-02-09 21:08:47 +0100

    Default texture fix

--- Ibis_Indicator_RoundHighlightSplit/Indicator.lua
+++ Ibis_Indicator_RoundHighlightSplit/Indicator.lua
@@ -81,7 +81,7 @@ function Instance:SetupHighlight(highlight)
 	highlight:SetPoint("TOPLEFT", self, "TOPLEFT")
 	highlight:SetPoint("BOTTOMRIGHT", self, "BOTTOMRIGHT")
 
-	local texture = self.config.texture or self.tracker.frameType:GetDefaultTexture() or nil
+	local texture = self.config.texture or self.tracker.frameType:GetDefaultTexture(self.parentFrame) or nil
 	highlight:SetTexture(texture)
 	highlight:SetBlendMode(self.config.blendMode or "ADD")
 	highlight:SetFrameStrata(self.config.strata or "MEDIUM")
